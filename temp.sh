6dc25dfa2a1010b35b149a16202b9e06c891077b
Allan Bolipata <bolipatc@mskcc.org>
2019-11-05 01:19:52 -0500

    Removed temp file

--- temp.sh
+++
@@ -1,5 +0,0 @@
-rm -rf ./flatbush_make_bam_and_qc/flatbush_qc/qc_fastqs/fastp_0.19.7/.git
-rm -rf ./flatbush_make_bam_and_qc/flatbush_qc/qc_fastqs/.git
-rm -rf ./flatbush_make_bam_and_qc/flatbush_qc/qc_bams/command_line_tools/alfred_0.1.17/.git
-rm -rf ./flatbush_make_bam_and_qc/flatbush_qc/qc_bams/command_line_tools/gatk_4.1.0.0/.git
-rm -rf ./flatbush_make_bam_and_qc/flatbush_qc/qc_bams/.git
